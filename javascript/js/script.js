7c051c69b4e7524816e08b23e3911ffb39aeb807
Anak1n1337 <odin.4450@gmail.com>
2023-11-01 10:52:35 -0300

    validando uf e cep

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -121,8 +121,74 @@ function validaCampoNumerico(elemento){
     });
 
 }
+
+function validaEmail(elemento) {
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+        
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+        
+        if(this.value.match(emailValido)){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+
+}
+
+function validaUf(elemento) {
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+        
+        const ufValido = /^[0-9]{2}$/;
+        
+        if(this.value.match(ufValido)){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+function validaCep(elemento) {
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+        
+        const cepValido = /^[0-9]{5}-[0-9]{3}$/;
+        
+        if(this.value.match(cepValido)){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
 let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
 let camposNumericos = document.querySelectorAll('input.numero');
+let camposEmail = document.querySelectorAll('input.email');
+let camposUf = document.querySelectorAll('input.uf');
+let camposCep = document.querySelectorAll('input.numerocep');
+
 
 for(let emFoco of camposObrigatorios) {
     validaCampo(emFoco)
@@ -130,4 +196,16 @@ for(let emFoco of camposObrigatorios) {
 
 for(let emFoco of camposNumericos) {
     validaCampoNumerico(emFoco)
+}
+
+for(let emFoco of camposEmail) {
+    validaEmail(emFoco)
+}
+
+for(let emFoco of camposUf) {
+    validaUf(emFoco)
+}
+
+for(let emFoco of camposCep) {
+    validaCep(emFoco)
 }
