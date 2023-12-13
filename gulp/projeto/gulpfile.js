a0fa4f712247315338e67e1feb712829451983b1
Anak1n1337 <odin.4450@gmail.com>
2023-12-13 12:45:00 -0300

    fim do modulo gulp1

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -3,12 +3,12 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const image = require('gulp-image')
+const image = require('gulp-imagemin')
 
 function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css', './vendor/owl/css/owl.css', './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css', './src/css/style.css'])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
@@ -17,8 +17,8 @@ function tarefasCSS(cb) {
 
 function tarefasJS(){
 
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/dist/js/bootstrap.js', './vendor/owl/js/owl.js', './vendor/jQueryMask/jquery.mask.js', './src/js/custom.js'])
+        .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'})) //libs.min.js
         .pipe(gulp.dest('./dist/js'))
