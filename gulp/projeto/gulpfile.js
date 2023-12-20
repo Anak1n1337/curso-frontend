53669f409f93e539a67ea9c0e5a2f1df043089aa
Anak1n1337 <odin.4450@gmail.com>
2023-12-20 09:27:36 -0300

    execicio gulp2

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,27 +1,41 @@
+const {series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
+// const stipCss = require('gulp-strip-css-comments')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 function tarefasCSS(cb) {
 
-    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css', './vendor/owl/css/owl.css', './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css', './src/css/style.css'])
+    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css', './vendor/owl/css/owl.css', './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css', './src/css/style.css'])
         .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
+    return cb()
+
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/dist/js/bootstrap.js', './vendor/owl/js/owl.js', './vendor/jQueryMask/jquery.mask.js', './src/js/custom.js'])
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min'})) //libs.min.js
-        .pipe(gulp.dest('./dist/js'))
+    gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/dist/js/bootstrap.js', './vendor/owl/js/owl.js', './vendor/jQueryMask/jquery.mask.js', './src/js/custom.js'])
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('scripts.js'))        // mescla arquivos
+        //.pipe(uglify())                  // minifica js
+        .pipe(rename({ suffix: '.min'}))   // scripts.min.js
+        .pipe(gulp.dest('./dist/js'))      // cria arquivos em novo diretório
+    
+        return callback()
 }
 
 
@@ -42,6 +56,31 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+// POC - Proof Of Concept
+function tarefasHTML(callback){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+}
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload) // repete o processo qundo alterar algo em src
+})
+
+// series x parallel
+const process = series(tarefasHTML, tarefasJS, tarefasCSS) 
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = process
+
